0e17df083d31a8da23053e8a33f62883751e3cdc
Jonas Nunes <105394156+jonasporpinoprof@users.noreply.github.com>
2022-05-11 21:28:10 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+document.write('Olá mundo!'+"<br>");
+//Comentário de linha única
+
+/*
+Comentário
+De várias linhas
+*/
+
+alert('dejesa continuar? ')
+var nome=prompt('Digite seu nome: ');
+var idade=prompt('Digite sua idade: ');
+document.write("Nome: <span style='color:#F00'>" + nome + "</span><br>");
+document.write("idade: <span style='color:#F00'>" + idade + "</span><br>");
